e6935be6badb0c2b05bf3f218d6891d1dc26ac54
Pavan Gaikwad <pavan.work.contact@gmail.com>
2023-06-22 19:11:41 +0530

    trying multiple tasks

--- dags/first.py
+++ dags/first.py
@@ -1,25 +1,43 @@
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from datetime import datetime
 
-
+# Define default arguments
 default_args = {
     "owner": "admin",
-    "start_date": datetime(2023, 1, 1),
+    "depends_on_past": False,
+    "start_date": datetime(2023, 6, 22),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
-with DAG(
-    dag_id="first",
-    description="My first DAG",
-    default_args=default_args,
-    schedule_interval="*/10 * * * *", # every 10 minutes
-    
-) as dag:
-    task1 = BashOperator(
-        task_id="task1",
-        bash_command="echo 'Hello World!'",
-    )
-    task2 = BashOperator(
-        task_id="task2",
-        bash_command="sleep 10",
-    )
+# Instantiate the DAG
+dag = DAG("docker_image_build", default_args=default_args, schedule_interval=None)
+
+# Define the Git repository URL and the Docker image name
+GIT_REPO_URL = "https://github.com/your-username/your-repo.git"
+DOCKER_IMAGE_NAME = "your-docker-hub-username/your-image-name:your-tag"
+
+# Define the tasks
+clone_repo = BashOperator(
+    task_id="clone_repo",
+    bash_command=f"echo 'git clone {GIT_REPO_URL} /tmp/repo' && sleep 10",
+    dag=dag,
+)
+
+build_docker_image = BashOperator(
+    task_id="build_docker_image",
+    bash_command="echo 'cd /tmp/repo/centos7 && docker build -t {} .'".format(DOCKER_IMAGE_NAME),
+    dag=dag,
+)
+
+upload_docker_image = BashOperator(
+    task_id="upload_docker_image",
+    bash_command="echo 'docker push {}'".format(DOCKER_IMAGE_NAME),
+    dag=dag,
+)
+
+# Define the task dependencies
+clone_repo >> build_docker_image >> upload_docker_image
