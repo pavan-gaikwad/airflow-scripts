693fc296ca991c6b3f625c6e9dff88e5d39be2ec
Pavan Gaikwad <pavan.work.contact@gmail.com>
2023-06-22 18:58:06 +0530

    trying multiple tasks

--- dags/first.py
+++ dags/first.py
@@ -12,9 +12,14 @@ with DAG(
     dag_id="first",
     description="My first DAG",
     default_args=default_args,
+    schedule_interval="*/10 * * * *", # every 10 minutes
     
 ) as dag:
     task1 = BashOperator(
         task_id="task1",
         bash_command="echo 'Hello World!'",
+    )
+    task2 = BashOperator(
+        task_id="task2",
+        bash_command="sleep 10",
     )
