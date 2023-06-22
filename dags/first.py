e5f90a8d6bebbad4405ad71967b49a3efa902ee7
Pavan Gaikwad <pavan.work.contact@gmail.com>
2023-06-22 18:50:17 +0530

    added start date

--- dags/first.py
+++ dags/first.py
@@ -3,14 +3,17 @@ from airflow.operators.bash_operator import BashOperator
 
 
 default_args = {
-    "owner": "admin"
+    "owner": "admin",
+    "start_date": datetime(2023, 1, 1),
 }
 
 with DAG(
     dag_id="first",
     description="My first DAG",
+    default_args=default_args,
+    
 ) as dag:
     task1 = BashOperator(
         task_id="task1",
-        bash_command="echo 'Hello World!'"
+        bash_command="echo 'Hello World!'",
     )
